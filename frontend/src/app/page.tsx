73a6d370cdc19f2ee042586aad9212abf663c59d
Alfredo Costa <alfredocosta@live.com>
2023-03-07 23:54:27 -0300

    Fix HTML conflict

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import styles from './page.module.css'
-import {Button, Card} from "react-bootstrap";
+import {Card} from "react-bootstrap";
 
 export default function Home() {
     return (
@@ -9,11 +8,9 @@ export default function Home() {
             <Card.Header>Notification System</Card.Header>
             <Card.Body>
                 <Card.Title>Gila Software</Card.Title>
-                <Card.Text>
-                    <p>This is a notification system that receives a message and depending on the category and subscribers, sends notifications to those users in the channels they are registered.</p>
-                    <p>The system does not actually send any messages or communicate with external APIs. Instead, it stores information about notifications in a log. The log includes details about the message, notification type, user data, time, and
-                        more.</p>
-                </Card.Text>
+                <p>This is a notification system that receives a message and depending on the category and subscribers, sends notifications to those users in the channels they are registered.</p>
+                <p>The system does not actually send any messages or communicate with external APIs. Instead, it stores information about notifications in a log. The log includes details about the message, notification type, user data, time, and
+                    more.</p>
             </Card.Body>
         </Card>
     )
